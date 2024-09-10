9150662d0aec5059163dee4c8440b225d9a6b6cb
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-09-10 19:33:14 +0200

    feat: setting up some more meta stuff

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,19 +1,30 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				white: {
+					background: "#DFDFDF",
+					primary: "#FFFFFF",
+				},
+				black: {
+					primary: "#000000",
+					secondary: "#101010",
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
+
+// TODO: FONTS
+// Bacasime Antique (logo)
+// Gotham (primary)
+// Monstserrat (secondary)
