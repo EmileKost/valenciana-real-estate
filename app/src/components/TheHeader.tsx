1fc4f0dd12946acc16f824674f659551c3c1a059
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-09-11 11:24:39 +0200

    feat: in and out animating of header

--- app/src/components/TheHeader.tsx
+++ app/src/components/TheHeader.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import { ROUTES } from "@/constants/routes";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 
 import { Navigation } from "./Navigation";
 
@@ -8,19 +12,33 @@ type TheHeaderProps = {
 	isDetailPage?: boolean;
 };
 
-export const TheHeader = ({ headerText, isDetailPage }: TheHeaderProps) => (
-	<header className="px-3 md:px-8 pt-10 pb-2 md:pt-20 md:pb-14 flex items-center justify-between w-full fixed top-0 left-0">
-		<div>
-			<Link href={ROUTES.HOME.href}>
-				{isDetailPage ? (
-					"Back"
-				) : (
-					<h1 className="font-text-serif text-black-primary text-xl md:text-3xl">
-						{headerText}
-					</h1>
-				)}
-			</Link>
-		</div>
-		<Navigation />
-	</header>
-);
+export const TheHeader = ({ headerText, isDetailPage }: TheHeaderProps) => {
+	const [isScrolling, setIsScrolling] = useState<boolean>(false);
+	const { scrollY } = useScroll();
+
+	useMotionValueEvent(scrollY, "change", (latest) => {
+		latest > 30 ? setIsScrolling(true) : setIsScrolling(false);
+	});
+
+	return (
+		<motion.header
+			animate={{
+				y: isScrolling ? "-100%" : "0",
+				opacity: isScrolling ? 0 : 1,
+			}}
+			className="px-3 md:px-8 pt-10 pb-2 md:pt-20 md:pb-14 flex items-center justify-between w-full fixed top-0 left-0">
+			<div>
+				<Link href={ROUTES.HOME.href}>
+					{isDetailPage ? (
+						"Back"
+					) : (
+						<h1 className="font-text-serif text-black-primary text-xl md:text-3xl">
+							{headerText}
+						</h1>
+					)}
+				</Link>
+			</div>
+			<Navigation />
+		</motion.header>
+	);
+};
