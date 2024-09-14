381fd0b46d039f70a7cee153d9bffa6d6589ed69
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-09-14 14:42:59 +0200

    feat: setting up skeleton getUser

--- app/auth.ts
+++ app/auth.ts
@@ -3,7 +3,23 @@ import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 
+const getUser = async (email: string) => {
+	try {
+		// Check in MongoDB database here for user
+	} catch (err) {
+		// Err handling
+	}
+};
+
 export const { auth, signIn, signOut } = NextAuth({
 	...authConfig,
-	providers: [Credentials({})],
+	providers: [
+		Credentials({
+			async authorize(credentials) {
+				const parsedCredentials = z
+					.object({ email: z.string().email(), password: z.string().min(6) })
+					.safeParse(credentials);
+			},
+		}),
+	],
 });
