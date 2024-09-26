aa25540ced5b26021751c813d9f4ce4c4acb3202
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-09-26 14:50:26 +0200

    feat: setting text and location

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -23,15 +23,24 @@ export default function Home() {
 						))}
 					</ListingsList>
 				) : null}
-				<div className="px-3 md:px-8 flex flex-col mt-8 md:mt-14">
-					<h2 className="font-heading text-2xl md:text-5xl text-black-primary font-bold">
-						Come find us!
-					</h2>
+				<div className="px-3 md:px-8 flex flex-col gap-4 md:gap-8 mt-8 md:mt-14">
+					<div>
+						<h2 className="font-heading text-2xl md:text-5xl text-black-primary font-bold">
+							Come find us!
+						</h2>
+						<p className="w-full md:max-w-[600px] mt-2 font-sans">
+							Visit our office in the heart of Valencia! We’re located in the
+							center of the city. Easily accessible by public transport and
+							close to major landmarks. We look forward to helping you with all
+							your real estate needs!
+						</p>
+					</div>
+
 					{/* Google maps Component */}
 					{/* - Using Valencia coords for now */}
 					<Map
-						lon={39.4738}
-						lat={0.3756}
+						lon={-0.37739}
+						lat={39.46975}
 					/>
 				</div>
 			</main>
--- app/src/components/Map.tsx
+++ app/src/components/Map.tsx
@@ -28,6 +28,8 @@ export const Map = ({ lon, lat }: MapProps) => {
 		refMap.current = new mapboxgl.Map({
 			// @ts-expect-error Weird map type to fix later
 			container: refMapContainer.current,
+			center: [lon, lat],
+			zoom: 10.12,
 		});
 
 		return () => {
@@ -35,13 +37,23 @@ export const Map = ({ lon, lat }: MapProps) => {
 				refMap.current.remove();
 			}
 		};
-	}, []);
+	}, [zoom, isError]);
 
 	return (
 		<div
 			// @ts-expect-error Map types for now are weird and need to be fixed
 			ref={refMapContainer}
-			className="w-full h-[60vh] md:h-[50vh] block bg-black-secondary"></div>
+			className="w-full h-[60vh] md:h-[50vh] block bg-black-secondary relative">
+			<div className="w-fit absolute right-3 bottom-3 flex flex-col">
+				<button className="w-10 h-12 rounded-t-full bg-black-secondary text-white-primary">
+					+
+				</button>
+				<div className="w-10 h-[1px] bg-white-primary" />
+				<button className="w-10 h-12 rounded-b-full bg-black-secondary text-white-primary">
+					-
+				</button>
+			</div>
+		</div>
 	);
 };
 
