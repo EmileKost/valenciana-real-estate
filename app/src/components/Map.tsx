a5e8f8076dab1cf51dd637d1c4d6cf38e91685d8
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-09-26 15:05:27 +0200

    feat: correctly handling error, settint dark style as test and handling annoying type errors

--- app/src/components/Map.tsx
+++ app/src/components/Map.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, LegacyRef } from "react";
 
 import mapboxgl from "mapbox-gl";
 
@@ -28,35 +28,55 @@ export const Map = ({ lon, lat }: MapProps) => {
 			return;
 		}
 
+		if (isError) return;
+
 		mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAP_KEY as string;
 		refMap.current = new mapboxgl.Map({
-			// @ts-expect-error Weird map type to fix later
-			container: refMapContainer.current,
+			container: refMapContainer.current as string | HTMLElement,
 			center: [coordinates.lon, coordinates.lat],
 			zoom: zoom,
+			style: "mapbox://styles/mapbox/dark-v11",
 		});
 
 		return () => {
 			if (refMap.current) {
 				refMap.current.remove();
+				setIsError(false);
 			}
+
+			return;
 		};
 	}, [zoom, isError]);
 
 	return (
-		<div
-			// @ts-expect-error Map types for now are weird and need to be fixed
-			ref={refMapContainer}
-			className="w-full h-[60vh] md:h-[50vh] block bg-black-secondary relative">
-			<div className="w-fit absolute right-3 bottom-3 flex flex-col">
-				<button className="w-10 h-12 rounded-t-full bg-black-secondary text-white-primary">
-					+
-				</button>
-				<div className="w-10 h-[1px] bg-white-primary" />
-				<button className="w-10 h-12 rounded-b-full bg-black-secondary text-white-primary">
-					-
-				</button>
-			</div>
+		<div className="w-full h-[60vh] md:h-[50vh] block bg-black-secondary">
+			{!isError ? (
+				<div
+					ref={refMapContainer as LegacyRef<HTMLDivElement> | undefined}
+					className="w-full h-full relative">
+					<div className="w-fit absolute right-3 bottom-3 flex flex-col">
+						<button className="w-10 h-12 rounded-t-full bg-black-secondary text-white-primary">
+							+
+						</button>
+						<div className="w-10 h-[1px] bg-white-primary" />
+						<button className="w-10 h-12 rounded-b-full bg-black-secondary text-white-primary">
+							-
+						</button>
+					</div>
+				</div>
+			) : (
+				<div className="w-full h-full flex items-center">
+					<div className="py-4 md:py-8">
+						<h3 className="font-heading text-white-primary text-xl md:text-5xl">
+							Map UnAvailable
+						</h3>
+						<p className="font-text text-white-primary text-lg md:text-xl">
+							Unfortunately the map could not be loaded, check your internet
+							connection, refresh and try again
+						</p>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
