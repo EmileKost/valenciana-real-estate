35a6f36d3e67bbbc436ea41383e21ec637bcfcf1
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-09-27 11:10:45 +0200

    fix: removing twMerge from darkblur comp

--- app/src/components/DarkBlur.tsx
+++ app/src/components/DarkBlur.tsx
@@ -1,10 +1,3 @@
-import { twMerge } from "tailwind-merge";
-
 export const DarkBlur = () => (
-	<div
-		className={twMerge(
-			"w-full h-full absolute top-0 left-0 z-40 bg-black-primary",
-			`opacity-20`
-		)}
-	/>
+	<div className="w-full h-full absolute top-0 left-0 z-40 bg-black-primary opacity-20" />
 );
