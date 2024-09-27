77b58f6590233e58014ef5058eb22e2afa88d471
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-09-27 11:05:14 +0200

    feat: error handling

--- app/src/components/Map.tsx
+++ app/src/components/Map.tsx
@@ -11,6 +11,7 @@ import {
 	MAXIMUM_ZOOM_IN_VALUE,
 } from "@/constants/zoomValue";
 
+import { MAP_STYLES } from "@/constants/mapStyles";
 import { Geojson, Pinpoint } from "@/types/map";
 
 type MapProps = {
@@ -64,7 +65,7 @@ export const Map = ({ lon, lat, geojson }: MapProps) => {
 			container: refMapContainer.current as string | HTMLElement,
 			center: [coordinates.lon, coordinates.lat],
 			zoom: zoom,
-			style: "mapbox://styles/mapbox/dark-v11",
+			style: MAP_STYLES.DARK,
 		});
 
 		refMap.current.on("load", () => {
@@ -94,29 +95,41 @@ export const Map = ({ lon, lat, geojson }: MapProps) => {
 	}, [zoom, isError, coordinates, geojson]);
 
 	return (
-		<div className="w-full h-[60vh] md:h-[50vh] block bg-black-secondary rounded-xl overflow-hidden relative">
-			<div
-				ref={refMapContainer as LegacyRef<HTMLDivElement> | undefined}
-				className="w-full h-full absolute top-0 left-0"></div>
-			<div className="w-fit absolute left-3 top-3 flex flex-col z-max">
-				<button
-					className="w-10 h-12 rounded-t-full bg-black-secondary text-white-primary"
-					onClick={() => handleUpdateZoomValue("plus")}>
-					+
-				</button>
-				<div className="w-10 h-[1px] bg-white-primary" />
-				<button
-					className="w-10 h-12 rounded-b-full bg-black-secondary text-white-primary"
-					onClick={() => handleUpdateZoomValue("minus")}>
-					-
-				</button>
-			</div>
+		<div className="w-full h-[60vh] md:h-[50vh] bg-black-secondary rounded-xl overflow-hidden relative flex items-center">
+			{!isError ? (
+				<>
+					<div
+						ref={refMapContainer as LegacyRef<HTMLDivElement> | undefined}
+						className="w-full h-full absolute top-0 left-0"></div>
+					<div className="w-fit absolute left-3 top-3 flex flex-col z-max">
+						<button
+							className="w-10 h-12 rounded-t-full bg-black-secondary text-white-primary"
+							onClick={() => handleUpdateZoomValue("plus")}>
+							+
+						</button>
+						<div className="w-10 h-[1px] bg-white-primary" />
+						<button
+							className="w-10 h-12 rounded-b-full bg-black-secondary text-white-primary"
+							onClick={() => handleUpdateZoomValue("minus")}>
+							-
+						</button>
+					</div>
+				</>
+			) : (
+				<div className="flex flex-col gap-2 md:gap-3 text-white-primary px-3 md:px-8">
+					<h3 className="font-heading text-xl md:text-4xl">Map unavailable</h3>
+					<p className="font-text text-sm md:text-lg">
+						Apologies, the map failed to load. Kindly refresh the page or try
+						again later.
+					</p>
+				</div>
+			)}
 		</div>
 	);
 };
 
 // MVP is Done
 // BACKLOG
-// - Switiching between places and smoothly animating this transition.
-// - *Custom marker (need a design and plan for that)
-// - BUG: Buttons dissapear when using styling from mapboxgl
+// - Switiching between places and smoothly animating this transition. (first need to think of an implementation for this)
+// - *Custom marker (need a design and plan for that) (JUST NEEDS A DESIGN);
+// - Better stylng error message
