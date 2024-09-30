910b2d79a0f158ac3cc7cb719700fee78aaa56b5
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-09-30 14:50:24 +0200

    feat: error handling

--- frontend/src/hooks/useGetAllProperties.tsx
+++ frontend/src/hooks/useGetAllProperties.tsx
@@ -3,6 +3,10 @@ import { useQuery } from "@tanstack/react-query";
 export const useGetAllProperties = () => {
 	const getAllProperties = async () => {
 		const response = await fetch("http://localhost:4500/properties");
+		if (!response.ok) {
+			throw new Error("Unable to fetch");
+		}
+
 		const result = await response.json();
 
 		return result;
