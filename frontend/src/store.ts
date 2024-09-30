55bc6ec1e72dd2843a315b5e6ed70a4226d0d935
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-09-30 13:57:05 +0200

    feat: setting up store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,11 +1,24 @@
 import { create } from "zustand";
 
-type UseFilterStore = {
+// Types
+import { Property } from "./types/property";
+
+type CurrencyStoreTypes = {
 	currency: string;
 	setCurrency: (newCurrency: string) => void;
 };
 
-export const useCurrencyStore = create<UseFilterStore>((set) => ({
+type PropertiesStoreTypes = {
+	properties: Property[];
+};
+
+export const useCurrencyStore = create<CurrencyStoreTypes>((set) => ({
 	currency: "EUR",
 	setCurrency: (newCurrency: string) => set({ currency: newCurrency }),
+}));
+
+export const usePropertiesStore = create<PropertiesStoreTypes>((set) => ({
+	properties: [],
+	setProperties: (allProperties: Property[]) =>
+		set({ properties: allProperties }),
 }));
---
+++ frontend/src/types/property.ts
@@ -0,0 +1,8 @@
+export type Property = {
+	id: number;
+	name: string;
+	location: string;
+	price: number;
+	type: string;
+	bedrooms: number;
+};
